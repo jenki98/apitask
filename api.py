9ff24bef6d162491d1ce8e457cddaf30db380a3f
jenki98 <zaynab.bawa@gmail.com>
2020-06-13 00:58:18 +0100

    displays users within 50 miles

--- api.py
+++ api.py
@@ -7,19 +7,22 @@ london = (51.509865, -0.118092)
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+def get_data():
+    x = 2
 
 @app.route('/', methods=['GET'])
-
 def home():
     return "<h1> Main page </h1> <p> To get <a href = http://127.0.0.1:5000/user_loc>users</a></p>"
 
-@app.route('/user_loc', methods=['GET'])
 
+@app.route('/user_loc', methods=['GET'])
 def get_users():
     global miles_dest
     r = requests.get("https://bpdts-test-app.herokuapp.com/users")
     data = r.json()
-
+    user_string = " "
+    users = {}
+    count = 0
     for items in data:
         first_name = items["first_name"]
         last_name = items["last_name"]
@@ -27,8 +30,10 @@ def get_users():
         longitude = items["longitude"]
         other_location = (latitude, longitude)
         miles_dest = int(distance.distance(london, other_location).miles)  # how to calcuate distance between two locations
-        print(latitude, longitude)
-        print(miles_dest)
-        user_string = first_name + " " + last_name + " - miles from London: " + str(miles_dest) + " miles"
-        return user_string
+        if miles_dest <= 50:
+            user_string = first_name + " " + last_name + " - miles from London: " + str(miles_dest) + " miles"
+            users[count] = user_string
+        count += 1
+    return users
+
 app.run()
