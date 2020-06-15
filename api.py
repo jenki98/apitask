cf5867fcecead2823b61a5a1e84ff9e2cab0538c
jenki98 <zaynab.bawa@gmail.com>
2020-06-15 21:39:09 +0100

    dynamic city and distance

--- api.py
+++ api.py
@@ -1,42 +1,52 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import requests
 import geopy
 import json
 from geopy.distance import geodesic
-
+import os
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1> Main page </h1> <p> To get <a href = http://127.0.0.1:5000/user_loc>users</a></p>"
+    return "<h1> Main page </h1> <p> To get <a href = http://127.0.0.1:5000/city/London/users>users</a></p>"
 
 
-@app.route('/user_loc/', methods=['GET'])
-def get_users():
-    geolocator = geopy.MapQuest("iBPa7S0BFc8HgAEMwEouQ6J6w4BwmvD2")
-    location = geolocator.geocode("London")
-    london = (location.latitude, location.longitude)
-    r_users = requests.get("https://bpdts-test-app.herokuapp.com/users")
+@app.route('/city/<city>/users', methods=['GET'])
+def get_users(city):
+    url = "https://bpdts-test-app.herokuapp.com/"
+    max_distance = float(request.args.get('max_distance', default=50))
+    mapquest_key = os.environ["MAPQUEST_KEY"] #mapquest key as environmental variable
+    geolocator = geopy.MapQuest(mapquest_key)
+    location = geolocator.geocode(city) #city from api parameter
+    city_coord = (location.latitude, location.longitude) #city coordinates from Map Quest
+
+    #retrieve user data from API
+    r_users = requests.get("{}/users".format(url))
     users_data = r_users.json()
-    r_city = requests.get("https://bpdts-test-app.herokuapp.com/city/London/users")
-    city_data = r_city.json()
-    cities = []
-    user = []
-    for city in city_data:
-        cities.append(city)
+    r_city = requests.get("{}/city/{}/users".format(url, city))
+    city_users = r_city.json()
+    city_users_data = []
+    users = []
 
-    for users in users_data:
-        other_location = (users["latitude"], users["longitude"])
-        miles_dest = int(geodesic(london, other_location).miles)  # how to calcuate distance between two locations
-        if miles_dest <= 50:
-            user.append(users)
-    city_users_dict = {city_user['id']: city_user for city_user in cities}
-    all_users = [] + cities
-    for users in user:
-        if users['id'] in city_users_dict:
-            print("de-duping")
+    for cities in city_users:
+        city_users_data.append(cities)
+
+    #filter results based on distance
+    for user in users_data:
+        other_location = (user["latitude"], user["longitude"])
+        miles_dest = float(geodesic(city_coord, other_location).miles)  # how to calcuate distance between two locations
+        if miles_dest <= max_distance:
+            users.append(user)
+
+    city_users_dict = {city_user['id']: city_user for city_user in city_users_data}
+    all_users = [] + city_users_data
+
+    #removing duplicates
+    for user in users:
+        if user['id'] in city_users_dict:
+            print("removing duplicates")
         else:
             all_users.append(users)
 
